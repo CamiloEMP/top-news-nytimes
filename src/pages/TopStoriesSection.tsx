7f4c8308fad126a40711ad5d473db10ba376a7e7
b7c8p <morapaezc@gmail.com>
2022-11-14 20:36:04 -0500

    add initial ui for page top-stories

--- src/pages/TopStoriesSection.tsx
+++ src/pages/TopStoriesSection.tsx
@@ -1,13 +1,67 @@
 import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
+import { Box, Center, Flex, Heading, Image, Text, Wrap, WrapItem } from '@chakra-ui/react'
 
-import { topNewSectionQuery } from '@/services/getTopNews'
+import { topStoriesSectionQuery } from '@/services/getTopStories'
 
 export const TopStoriesSection = () => {
   const params = useParams()
-  const { data: topNews, isLoading } = useQuery(topNewSectionQuery(params.section!))
+  const { data: topStories, isLoading } = useQuery(topStoriesSectionQuery(params.section!))
 
-  console.log(topNews)
+  console.log(topStories)
 
-  return <>{isLoading ? <div>cargando...</div> : <div>Top News {params.section}</div>}</>
+  return (
+    <>
+      {isLoading ? (
+        <div>cargando...</div>
+      ) : (
+        <section>
+          <Heading
+            as="h2"
+            bg="blackAlpha.400"
+            fontWeight="bold"
+            textAlign="center"
+            textTransform="capitalize"
+          >
+            {params.section === 'home' ? 'Last Top Stories' : params.section}
+          </Heading>
+          <Wrap as="div" justify="center" spacing="8px">
+            {topStories?.results.map(story => (
+              <WrapItem
+                key={story.url}
+                borderWidth="1px"
+                display="flex"
+                flexDirection="column"
+                gap={4}
+                justifyContent="space-between"
+                p={4}
+                rounded="md"
+                shadow="md"
+                w={['320px', '380px']}
+              >
+                <Text fontSize="xl" fontWeight="semibold">
+                  {story.title}
+                </Text>
+                <Text>{story.abstract}</Text>
+                <Image
+                  alt={story.multimedia[0].caption}
+                  boxSize={['250px', '280px']}
+                  mx="auto"
+                  objectFit="cover"
+                  rounded="md"
+                  src={story.multimedia[0].url}
+                />
+                <Flex direction="column">
+                  <Text fontSize="small">
+                    Published date: {new Date(story.published_date).toLocaleDateString()}
+                  </Text>
+                  <Text fontSize="small">{story.byline}</Text>
+                </Flex>
+              </WrapItem>
+            ))}
+          </Wrap>
+        </section>
+      )}
+    </>
+  )
 }
